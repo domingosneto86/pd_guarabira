f48ddb97d23ef9881b1104c7ffe1589731fed4a0
domingosneto86 <domingosneto86@gmail.com>
2023-08-19 22:05:54 -0300

    adição do slider

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
 function main() {
     document.body.innerHTML = ''
-    buildHeader()
 
     main_div = document.createElement('div')
     main_div.id = 'main_div'
     main_div.className = 'main_div'
     document.body.appendChild(main_div)
+    buildHeader()
 
+    //CONTENT
+    div_content = document.createElement('div')
+    div_content.id = 'div_content'
+    div_content.className = 'div_content'
+    main_div.appendChild(div_content)
 
-    buildBody()
+    buildInicio()
     buildFooter()
+
 }
 
 function buildHeader() {
     header = document.createElement('header')
     header.id = 'header'
     header.className = 'header'
-    document.body.appendChild(header)
+    main_div.appendChild(header)
 
     div_logo_header = document.createElement('div')
     div_logo_header.id = 'div_logo_header'
@@ -35,22 +41,91 @@ function buildHeader() {
     header.appendChild(div_menu)
 
     list_options_menu = ['INÍCIO', 'O QUE É', 'AGENDA', 'PARTICIPAÇÃO SOCIAL', 'PRODUTOS', 'PARTICIPE JÁ', 'DÚVIDAS', 'NOTÍCIAS']
-    for (i in list_options_menu) {
+    list_builders = [buildInicio]
+        //for (i in list_options_menu) {
+    list_options_menu.forEach(function(option, i) {
         div_menu_item = document.createElement('div')
-        div_menu_item.id = 'div_menu_item'
+        div_menu_item.id = 'div_menu_item_' + i
         div_menu_item.className = 'div_menu_item'
         div_menu_item.innerHTML = list_options_menu[i]
         div_menu.appendChild(div_menu_item)
-    }
+
+        /* div_menu_item.onclick = function() {
+            console.log(option, i)
+            list_builders[i]()
+        } */
+    })
 }
 
-function buildBody() {
+
+
+function buildInicio() {
+    div_content.innerHTML = ''
+
+    //SLIDER DE RESUMO
+    //Carousel
+    carousel = document.createElement('div')
+    carousel.id = 'carousel'
+    carousel.className = 'carousel'
+    div_content.appendChild(carousel)
+
+    slides = document.createElement('div')
+    slides.id = 'slides'
+    slides.className = 'slides'
+    carousel.appendChild(slides)
+
+
+    div_slider_inicio = document.createElement('div')
+    div_slider_inicio.id = 'div_slider_inicio'
+    div_slider_inicio.className = 'slide'
+    div_slider_inicio.innerHTML = 'Início'
+    div_slider_inicio.setAttribute('data-state', 'active');
+    slides.appendChild(div_slider_inicio)
+
+    div_slider_timeline = document.createElement('div')
+    div_slider_timeline.id = 'div_slider_timeline'
+    div_slider_timeline.className = 'slide'
+    div_slider_timeline.innerHTML = 'Timeline'
+    slides.appendChild(div_slider_timeline)
+
+    div_slider_noticias = document.createElement('div')
+    div_slider_noticias.id = 'div_slider_noticias'
+    div_slider_noticias.className = 'slide'
+    div_slider_noticias.innerHTML = 'Noticias'
+    slides.appendChild(div_slider_noticias)
+
+    //Indicators
+    indicators = document.createElement('div')
+    indicators.id = 'indicators'
+    indicators.className = 'indicators'
+    carousel.appendChild(indicators)
+
+    indicator = document.createElement('radio')
+    indicator.name = 'indicator'
+    indicator.className = 'indicator'
+    indicator.setAttribute('data-slide', '1');
+    indicator.setAttribute('data-state', 'active');
+    indicators.appendChild(indicator)
+
+    indicator = document.createElement('radio')
+    indicator.name = 'indicator'
+    indicator.className = 'indicator'
+    indicator.setAttribute('data-slide', '2');
+    indicators.appendChild(indicator)
+
+    indicator = document.createElement('radio')
+    indicator.name = 'indicator'
+    indicator.className = 'indicator'
+    indicator.setAttribute('data-slide', '2');
+    indicators.appendChild(indicator)
+
+
     //INÍCIO
     div_inicio = document.createElement('div')
     div_inicio.id = 'div_inicio'
     div_inicio.className = 'div_inicio'
     div_inicio.style.backgroundImage = 'url(images/panoramica_inicio.png)'
-    main_div.appendChild(div_inicio)
+    div_content.appendChild(div_inicio)
 
     div_inicio_desc = document.createElement('div')
     div_inicio_desc.id = 'div_inicio_desc'
@@ -63,7 +138,7 @@ function buildBody() {
     div_overview.id = 'div_overview'
     div_overview.className = 'div_overview'
         //div_overview.style.backgroundImage = 'url(images/003.png)'
-    main_div.appendChild(div_overview)
+    div_content.appendChild(div_overview)
 
     div_overview_content = document.createElement('div')
     div_overview_content.id = 'div_overview_content'
@@ -95,6 +170,9 @@ function buildBody() {
     div_overview_img.style.backgroundImage = 'url(images/003.png)'
     div_overview.appendChild(div_overview_img)
 
+
+    playCarousel()
+
     //O QUE É
     /* div_about = document.createElement('div')
     div_about.id = 'div_about'
@@ -116,5 +194,5 @@ function buildFooter() {
     footer.id = 'footer'
     footer.className = 'footer'
     footer.style.backgroundImage = 'url(images/papel_timbrado_02.png)'
-    main_div.appendChild(footer)
+    div_content.appendChild(footer)
 }
